7e48ea23920dcb33a9c15e0abb8f3446585d7a7b
Haleh_Ghoreishi <h72ghorashi@gmail.com>
2021-10-26 10:45:33 +0330

    Add gsap to animate the ball

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,64 @@
 <template>
   <div id="app">
-      <h3>Bounce the Ball!</h3>
-  <transition
-    name="ballmove"
-    enter-active-class="bouncein"
-    leave-active-class="rollout" 
-   >
-  <div v-if="show">
-    <bouncing-ball class="child"></bouncing-ball>
-  </div>
-  </transition>
+    <h3>Bounce the Ball!</h3>
+    <button @click="show = !show">
+      Toggle
+    </button>
+    <transition
+      name="ballmove"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+      :css="false"
+    >
+      <bouncing-ball v-if="show" class="child"></bouncing-ball>
+    </transition>
   </div>
 </template>
 
 <script>
 import BouncingBall from './components/BouncingBall.vue';
+import gsap from 'gsap';
 
 export default {
   name: 'App',
-   components: {
-    BouncingBall,
+  components: {
+    BouncingBall
   },
   data() {
     return {
-      show: false 
+      show: false
+    };
+  },
+  mounted() {
+    this.show = true;
+    setTimeout(() => {
+      this.show = false;
+    }, 1500);
+  },
+  methods: {
+    beforeEnter(el) {
+      el.style.opacity = 0;
+      el.style.transform = 'translateY(0)';
+    },
+    enter(el, done) {
+      gsap.to(el, {
+        duration: 1,
+        y: 400,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+      });
+    },
+    leave(el, done) {
+      gsap.to(el, {
+        duration: 3,
+        x: 600,
+        rotate: 360,
+        ease: 'out',
+        onComplete: done
+      });
     }
-  },
-  mounted(){
-      this.show = true;
-    
-      setTimeout(()=>{
-        this.show= false;
-      },3000)
-  },
-}
+  }
+};
 </script>
